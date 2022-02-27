ac36f55e8551ea417f539c8513c44884cafbce2d
Myou5uf <myxamed12l@gmail.com>
2022-02-27 23:56:11 +0700

    Implemented data output to the page, filtering, search

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
-    const navLinks = document.querySelectorAll('.navigation-link'); // Ссылки в навигации
+    const navLinks = document.querySelectorAll(".navigation-link"); // Ссылки в навигации
+    const moreLink = document.querySelector(".more");
 
     /*  1-й способ написания
         // Метод для получения данных от серера (или с локального файла)
@@ -14,26 +15,86 @@ const getGoods = () => {
                 console.log(data);
             });
     */
-   
+
+    // Отрисовка карточек товаров
+    const renderGoods = (data) => {
+        const cardContainer = document.querySelector(".long-goods-list"); // Контейнер карточек с товарами
+
+        cardContainer.innerHTML = "";
+
+        // Переберем массив data (element - итерируемый элемент)
+        data.forEach((element) => {
+            // Создаем новый блок div
+            const cardBlock = document.createElement("div");
+            // Добавляем новый класс к новому блоку
+            cardBlock.classList.add("col-lg-3");
+            cardBlock.classList.add("col-sm-6");
+
+            //  Запишем в новый блок верстку карточки товара
+            cardBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${element.label ? null : "d-none"}">${element.label}</span>
+                    <img src="db/${element.img}" alt="${element.name}" class="goods-image" />
+                    <h3 class="goods-title">${element.name}</h3>
+                    <p class="goods-description">${element.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="007">
+                        <span class="button-price">$${element.price}</span>
+                    </button>
+                </div>
+            `;
+            cardContainer.append(cardBlock);
+            // console.log(data);
+        });
+    };
+
     // Функция возвращает данные с бд
-    const getData = () => {
+    const getData = (category, value) => {
         //  2-й способ написания
         fetch("/db/db.json")
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
+                // Метод filter возвращает новый массив из элементов,callback которых возвращает true
+                // Если category вернет true, то фильтруем данные, если false ,то возвращаем вcю data
+                const array = category ? data.filter((item) => item[category] === value) : data;
                 // Отправить полученные данные в локальное хранилище
-                localStorage.setItem('data', JSON.stringify(data)); 
+                localStorage.setItem("data", JSON.stringify(array));
+
+                // Если мы не находимся на странице goods.html, то переходим на эту страницу
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = "/goods.html";
+                } else {
+                    // Иначе запускаем функцию renderGoods с полученными данными
+                    renderGoods(array);
+                }
             });
     };
 
-     // Метод forEach аргументом принимает ф-ию
+    //
+
+    // Метод forEach аргументом принимает ф-ию
     navLinks.forEach((link) => {
-        link.addEventListener('click', (event) =>{
+        link.addEventListener("click", (event) => {
             event.preventDefault(); // Заблокируем стандартное поведение ссылок (это переход по url - href="#")
-            getData();  // При клике по категоримем запрашиваем данные с сервера( в нашем случае с файла json)
+            const category = link.dataset.field; // Значение из data - атрибута
+            const linkValue = link.textContent; // Текстовое содержимое ссылки
+            // При клике по категоримем запрашиваем данные с сервера( в нашем случае с файла json)
+            getData(category, linkValue);
         });
     });
+
+    // Если в локальном хранилище есть данные и находимся на странице goods.html
+    if (localStorage.getItem("data") && window.location.pathname === "/goods.html") {
+        // то запускаем renderGoods, передав данные из локального хранилища, предварительно распарсив их
+        renderGoods(JSON.parse(localStorage.getItem("data")));
+    }
+
+    // Переход в каталог товаров по кнопке View All
+    if (moreLink) {
+        // Если кнопка есть то
+        moreLink.addEventListener("click", () => {
+            getData();
+        });
+    }
 
     /* ---------------- Примеры использования localStorage
          localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
@@ -51,6 +112,6 @@ const getGoods = () => {
         localStorage.removeItem('goods');
         console.log(localStorage);
     */
-   
 };
+
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,19 +1,74 @@
 const search = function () {
-    const cartBtn = document.querySelector('.button-btn');  // Кнопка корзина
-    const input = document.querySelector('.search-block > input'); // Текстовое поле 
-    const searchBtn = document.querySelector('.search-block > button'); // Кнопка поиска 
+    const cartBtn = document.querySelector(".button-btn"); // Кнопка корзина
+    const input = document.querySelector(".search-block > input"); // Текстовое поле
+    const searchBtn = document.querySelector(".search-block > button"); // Кнопка поиска
 
-
-    // Событие input - ловит каждый ввод в наше поле вводе 
-    input.addEventListener('input', (event) => {
+    // Событие input - ловит каждый ввод в наше поле вводе
+    input.addEventListener("input", (event) => {
         console.log("Ввод в input");
         // console.log(event.target);    // event.target - хранит тот input на котом произошло событие
-        console.log(event.target.value);    // event.target.value - значение, введенное в поле ввода
+        // console.log(event.target.value); // event.target.value - значение, введенное в поле ввода
     });
 
-    // При клике на кнопку - поиск, выводим в консоль значение, введеное в поле для ввода 
-    searchBtn.addEventListener('click', () => {
-        console.log("Введенное значение: ", input.value);
+    // Отрисовка карточек товаров
+    const renderGoods = (data) => {
+        const cardContainer = document.querySelector(".long-goods-list"); // Контейнер карточек с товарами
+
+        cardContainer.innerHTML = "";
+
+        // Переберем массив data (element - итерируемый элемент)
+        data.forEach((element) => {
+            // Создаем новый блок div
+            const cardBlock = document.createElement("div");
+            // Добавляем новый класс к новому блоку
+            cardBlock.classList.add("col-lg-3");
+            cardBlock.classList.add("col-sm-6");
+
+            //  Запишем в новый блок верстку карточки товара
+            cardBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${element.label ? null : "d-none"}">${element.label}</span>
+                    <img src="db/${element.img}" alt="${element.name}" class="goods-image" />
+                    <h3 class="goods-title">${element.name}</h3>
+                    <p class="goods-description">${element.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="007">
+                        <span class="button-price">$${element.price}</span>
+                    </button>
+                </div>
+            `;
+            cardContainer.append(cardBlock);
+        });
+    };
+
+    // Функция возвращает данные с бд
+    const getData = (value) => {
+        //  Запрашиваем данные из БД
+        fetch("/db/db.json")
+            .then((response) => response.json()) // Получаем данные в виде json
+            .then((data) => {
+                // Фильтруем data
+                const array = data.filter((item) => {
+                    // возвращем те карточки, у которых name равен value (введеное пользователем значение в поле ввода)
+                    // Метод includes() определяет, содержит ли массив определённый элемент, возвращая в зависимости от этого true или false
+                    // Чтобы не зависить от регистра преобразуем name и value в нижний регистр
+                    return item.name.toLowerCase().includes(value.toLowerCase());
+                });
+                console.log("Полученное значение из input: ", value);
+                // Отправить полученные данные в локальное хранилище
+                localStorage.setItem("data", JSON.stringify(array));
+
+                // Если мы не находимся на странице goods.html, то переходим на эту страницу
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = "/goods.html";
+                } else {
+                    // Иначе запускаем функцию renderGoods с полученными данными
+                    renderGoods(array);
+                }
+            });
+    };
+
+    searchBtn.addEventListener("click", () => {
+        getData(input.value);
     });
 };
 
